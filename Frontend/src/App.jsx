343d366a0f4e583897328ee3eadb0b4f9fb2c26f
Upendra Komaripati <upendrakomaripaticse@gmail.com>
2025-12-09 23:47:59 +0530

    Updated in App Component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,47 @@
 import React from "react";
-import Login from './Pages/Login'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Home from "./Pages/Home";
-import Register from "./Pages/Register";
-import { UserData } from "./Context/User";
-import Admin from "./Pages/Admin"
+import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import { UserData } from "./context/User";
+import Loading from "./components/Loading";
+import Admin from "./pages/Admin";
+import PlayList from "./pages/PlayList";
+import Album from "./pages/Album";
 
-function App() {
-const {user, isAuth}=UserData()
+const App = () => {
+  const { loading, user, isAuth } = UserData();
   return (
     <>
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={ isAuth?<Home/>:<Login/>}></Route>
-      <Route path="/Admin" element={ isAuth?<Home/>:<Admin/>}></Route> 
-      <Route path="/login" element={ isAuth?<Home/>:<Login/>}></Route>
-      <Route path="/register" element={isAuth?<Home/>:<Register/>}></Route>
-    </Routes>
-    </BrowserRouter>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={isAuth ? <Home /> : <Login />} />
+            <Route
+              path="/playlist"
+              element={isAuth ? <PlayList user={user} /> : <Login />}
+            />
+            <Route
+              path="/album/:id"
+              element={isAuth ? <Album user={user} /> : <Login />}
+            />
+            <Route
+              path="/playlist"
+              element={isAuth ? <PlayList user={user} /> : <Login />}
+            />
+            <Route path="/admin" element={isAuth ? <Admin /> : <Login />} />
+            <Route path="/login" element={isAuth ? <Home /> : <Login />} />
+            <Route
+              path="/register"
+              element={isAuth ? <Home /> : <Register />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
